b36e3ca25ededd26afa332c528687004a91fb164
hasibprangon <hasibulhossainp09@gmail.com>
2025-05-08 15:10:12 +0600

    first problem done

--- src/assignmentProblem.ts
+++ src/assignmentProblem.ts
@@ -1,6 +1,15 @@
 {
     // 
+    function formatString(input: string, toUpper?: boolean): string{
+        if(toUpper === false){
+            return input.toLowerCase()
+        }else{
+            return input.toUpperCase()
+        }
+    }
 
+    // const result1 = formatString('Hridoy', );
+    // console.log(result1);
 
 
     // 
